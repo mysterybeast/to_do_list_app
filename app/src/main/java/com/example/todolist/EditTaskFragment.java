cac1e2cace7ef27e8fabe1167d67ab78db5d8839
Sultan Zverev <zverevsultan@gmail.com>
2024-12-18 01:56:49 +0700

    Used popBackStack and fixed the condition &&

--- app/src/main/java/com/example/todolist/EditTaskFragment.java
+++ app/src/main/java/com/example/todolist/EditTaskFragment.java
@@ -12,79 +12,57 @@ import androidx.fragment.app.Fragment;
 import com.example.todolist.Data.DataBaseHandler;
 import com.example.todolist.Model.Task;
 
+
 public class EditTaskFragment extends Fragment {
 
-    private EditText mHeader, mDescription;
-    private ImageButton mBackImageButton;
-    private DataBaseHandler mDataBaseHandler;
-    private Task mTask;
-    private int mTaskId;
+    private EditText header, description;
+    private ImageButton backImageButton;
+    private DataBaseHandler dataBaseHandler;
+    private Task task;
+    private int taskId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_task, container, false);
 
-        mDataBaseHandler = new DataBaseHandler(getActivity());
+        dataBaseHandler = new DataBaseHandler(getActivity());
 
-        mHeader = view.findViewById(R.id.header);
-        mDescription = view.findViewById(R.id.description);
-        mBackImageButton = view.findViewById(R.id.backImageButton);
+        header = view.findViewById(R.id.header);
+        description = view.findViewById(R.id.description);
+        backImageButton = view.findViewById(R.id.backImageButton);
 
         Bundle bundle = getArguments();
+        if (bundle != null) taskId = bundle.getInt("id");
 
-        if (bundle != null) mTaskId = bundle.getInt("id");
-
-        if (mTaskId > 0) {
-            mTask = new Task();
-            mTask = mDataBaseHandler.getTask(mTaskId);
-            mHeader.setText(mTask.getHeader());
-            mDescription.setText(mTask.getDescription());
+        if (taskId > 0) {
+            task = dataBaseHandler.getTask(taskId);
+            header.setText(task.getHeader());
+            description.setText(task.getDescription());
         }
 
-        mBackImageButton.setOnClickListener(v -> {
-            TaskListFragment taskListFragment = new TaskListFragment();
-            TaskDetailFragment taskDetailFragment = new TaskDetailFragment();
-
-            if (mTaskId > 0) {
-                if (isFieldsChanged() & isFieldsNotEmpty()) { //обновляем если изменили поля
-                    mTask = new Task(mTaskId, mHeader.getText().toString(),
-                            mDescription.getText().toString());
-                    mDataBaseHandler.updateTask(mTask);
-                }
-
-                Bundle bundle1 = new Bundle();
-                bundle1.putInt("id", mTaskId);
-                taskDetailFragment.setArguments(bundle1);
-
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, taskDetailFragment)
-                        .addToBackStack(null)
-                        .commit();
-            } else {
-                if (isFieldsNotEmpty()) {
-                    mTask = new Task(mHeader.getText().toString(),
-                            mDescription.getText().toString());
-                    mDataBaseHandler.addTask(mTask);
-                }
-
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, taskListFragment)
-                        .addToBackStack(null)
-                        .commit();
+        backImageButton.setOnClickListener(v -> {
+            if (taskId > 0 && isFieldsChanged() && isFieldsNotEmpty()) {
+                task = new Task(taskId, header.getText().toString(),
+                        description.getText().toString());
+                dataBaseHandler.updateTask(task);
+            } else if (taskId == 0 && isFieldsNotEmpty()) {
+                task = new Task(header.getText().toString(),
+                        description.getText().toString());
+                dataBaseHandler.addTask(task);
             }
+            getParentFragmentManager().popBackStack();
         });
-
         return view;
     }
 
     boolean isFieldsNotEmpty() {
-        return (!mHeader.getText().toString().isBlank() &
-                !mDescription.getText().toString().isBlank());
+        return (!header.getText().toString().isBlank() &
+                !description.getText().toString().isBlank());
     }
 
     boolean isFieldsChanged() {
-        return !(mHeader.getText().toString().equals(mTask.getHeader())) |
-                !(mDescription.getText().toString().equals(mTask.getDescription()));
+        return !(header.getText().toString().equals(task.getHeader())) |
+                !(description.getText().toString().equals(task.getDescription()));
     }
 }
--- app/src/main/java/com/example/todolist/TaskAdapter.java
+++ app/src/main/java/com/example/todolist/TaskAdapter.java
@@ -21,59 +21,44 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
         void onTaskClick(Task task, int position);
     }
 
-    private final OnTaskClickListener mOnClickListener;
-    private final LayoutInflater mInflater;
-    private final List<Task> mTasks;
+    private final OnTaskClickListener onClickListener;
+    private final LayoutInflater inflater;
+    private final List<Task> tasks;
 
     TaskAdapter(Context context, List<Task> tasks, OnTaskClickListener onClickListener) {
-        this.mOnClickListener = onClickListener;
-        this.mTasks = tasks;
-        this.mInflater = LayoutInflater.from(context);
+        this.onClickListener = onClickListener;
+        this.tasks = tasks;
+        this.inflater = LayoutInflater.from(context);
     }
-
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        final ViewHolder viewHolder;
-//
-//        if (convertView == null) {
-//            convertView = inflater.inflate(this.layout, parent, false);
-//            viewHolder = new ViewHolder(convertView);
-//            convertView.setTag(viewHolder);
-//        } else {
-//            viewHolder = (ViewHolder) convertView.getTag();
-//        }
-//        return convertView;
-//    }
 
     @NonNull
     @Override
     public TaskAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.task_layout, parent, false);
+        View view = inflater.inflate(R.layout.task_layout, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(TaskAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
-        Task task = mTasks.get(position);
+        Task task = tasks.get(position);
         holder.header.setText(task.getHeader());
         holder.description.setText(task.getDescription());
-        holder.itemView.setOnClickListener(v -> mOnClickListener.onTaskClick(task, position));
+        holder.itemView.setOnClickListener(v -> onClickListener.onTaskClick(task, position));
     }
 
     @Override
     public int getItemCount() {
-        return mTasks.size();
+        return tasks.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView header, description;
-
         //CheckBox checkbox;
         ViewHolder(View view) {
             super(view);
             header = view.findViewById(R.id.header);
             description = view.findViewById(R.id.description);
             //checkbox = view.findViewById(R.id.checkbox);
-
         }
     }
 }
